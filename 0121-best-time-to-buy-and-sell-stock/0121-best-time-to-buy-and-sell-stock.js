1f3b0f6352f3c383343a6ac1eec88348cc236b12
Hanna <112612098+hannavas2022@users.noreply.github.com>
2023-12-16 20:08:26 -0600

    Time: 59 ms (96.53%), Space: 51.7 MB (43.61%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  var n = prices.length;
+  var minPrice = Number.MAX_SAFE_INTEGER;
+  var maxProfit = 0;
+  for (var i = 0; i < n; i++) {
+    if (prices[i] < minPrice) minPrice = prices[i];
+    else if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice;
+  }
+  return maxProfit;  
+};
