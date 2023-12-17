432c51eaf0be7037e523baeb5f73f14854dbfb26
Hanna <112612098+hannavas2022@users.noreply.github.com>
2023-12-16 20:06:23 -0600

    Time: 64 ms (98.82%), Space: 51.4 MB (69.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -7,7 +7,7 @@ var rotate = function(nums, k) {
    const len = nums.length
     k = (k % len)
     
-    let end = nums.splice(-k, k)
+    let end = nums.splice(len - k)
 
     nums.splice(0,0,...end)
 };
